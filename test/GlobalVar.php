39f2934eb9e926889d9dc383d9b6c2c9a8a41070
Bruno Pedro <bpedro@brunopedro.com>
2014-11-21 12:17:48 +0100

    [FIX] Check if value exists before filtering it.

--- test/GlobalVar.php
+++ test/GlobalVar.php
@@ -5,16 +5,28 @@ class GlobalVar
 {
     public function getGet($name)
     {
-        return filter_var($_GET[$name]);
+        if (!empty($_GET[$name])) {
+            return filter_var($_GET[$name]);
+        } else {
+            return null;
+        }
     }
 
     public function getPost($name)
     {
-        return filter_var($_POST[$name]);
+        if (!empty($_POST[$name])) {
+            return filter_var($_POST[$name]);
+        } else {
+            return null;
+        }
     }
 
     public static function getEnv($name)
     {
-        return filter_var($_ENV[$name]);
+        if (!empty($_ENV[$name])) {
+            return filter_var($_ENV[$name]);
+        } else {
+            return null;
+        }
     }
 }
